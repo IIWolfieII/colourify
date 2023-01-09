eb7089e64648656db8e2f9bc882795d99c2a4d77
IIWolfieII <iiwolfieii@yahoo.com>
2023-01-09 19:33:21 +0000

    Initial Base Setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SideBar from "@/components/SideBar.vue";
+</script>
+
+<template>
+  <div class="flex flex-no-wrap h-screen" style="background-color: #001514;">
+    <SideBar></SideBar>
+    <div class="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+      <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border -->
+      <div class="w-full h-full rounded border-dashed border-2 border-gray-300">
+        <!-- Place your content here -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TheWelcome from './../components/TheWelcome.vue'
+</script>
+
+<template>
+  <TheWelcome/>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  mounted() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
